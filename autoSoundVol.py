0c4313777001795a579e0351c0f652ee70e80347
krisiol23 <krys.wis78@onet.pl>
2022-06-22 21:06:44 +0200

    initial commit

---
+++ autoSoundVol.py
@@ -0,0 +1,19 @@
+import os
+import keyboard 
+import subprocess
+import signal
+import time
+
+while(True):
+    try:
+        time.sleep(0.05)
+        if keyboard.is_pressed('alt+5'):   
+            si = subprocess.STARTUPINFO()
+            si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            subprocess.Popen('taskkill /F /IM cmd.exe', startupinfo=si)
+            os.system("SndVol.exe")
+            time.sleep(0.05)
+            continue
+    except:
+        continue
+
